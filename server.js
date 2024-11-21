cd880c527b6899e926c3511c294a825e5b2296f0
Your Name <you@example.com>
2024-11-21 21:48:33 +0100

    3/5

---
+++ server.js
@@ -0,0 +1,93 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5555;
+
+const users = [];
+
+function checkMustChangePassword(req, res, next) {
+  const { email } = req.body;
+  const user = users.find((user) => user.email === email);
+
+  if (user && user.mustChangePassword) {
+    return res.status(403).json({
+      error: "вам нужно сменить пароль перед дальнейшим использованием.",
+    });
+  }
+  next();
+}
+
+app.post("/register", checkMustChangePassword, async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "не все данные введены" });
+  }
+
+  if (users.find((user) => user.email === email)) {
+    return res.status(400).json({ error: "этот и-мейл уже зарегистрирован" });
+  }
+
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    users.push({ email, password: hashedPassword, mustChangePassword: false });
+
+    res.status(201).send("пользователь зарегистрирован!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("ошибка регистрации");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((user) => user.email === email);
+  if (!user) {
+    return res.status(404).json({ error: "пользователь не найден" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({ error: "неверный пароль" });
+  }
+
+  res.status(200).json({ message: "вход успешен!" });
+});
+
+app.post("/change-password", async (req, res) => {
+  const { email, newPassword } = req.body;
+
+  if (!email || !newPassword) {
+    return res.status(400).json({ error: "почта и новый пароль обязательны!" });
+  }
+
+  const user = users.find((user) => user.email === email);
+  if (!user) {
+    return res.status(404).json({ error: "пользователь не найден" });
+  }
+
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+    user.password = hashedPassword;
+    user.mustChangePassword = false;
+    res.status(200).json({ message: "пароль успешно изменен" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "ошибка сервера при смене пароля" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`сервер работает на порту: ${port}`);
+});
